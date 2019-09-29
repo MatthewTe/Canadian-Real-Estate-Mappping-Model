c3a57c7a7f8b79d88fbcac37baaf5cd1614db217
MatthewTe <teelucksingh.matthew1@gmail.com>
2019-09-29 15:32:51 -0400

    Working on Webscraping.py

--- Toronto_Pipeline/data_writer/Web_Scraping.py
+++ Toronto_Pipeline/data_writer/Web_Scraping.py
@@ -1,8 +1,16 @@
+# Importing system and PATH management packages:
+import sys
+from os.path import dirname
+
 # Importing web scraping packages:
 import requests
 import bs4
+from selenium import webdriver
+
 # Importing data management packages:
 import pandas as pd
+
+# PATH management:
 
 
 class Point_2_Homes(object):
@@ -30,14 +38,97 @@ class Point_2_Homes(object):
             Dataframe containing all real estate listings data from the webpage
         '''
 
+        # WARNING: Code unstalbe due to websites use of Incapsula protection:
+
+
         # Connecting to webpage:
-        res = requests.get(url)
+        # Using selenium to bypass Incapsula:
+
+        # Header to request for Incapsula:
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)\
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+        res = requests.get(url, headers=headers)
+
         soup = bs4.BeautifulSoup(res.text, 'lxml')
 
         # Seaching for all instances of listings article {class : item-cnt clearfix gold}
-        # TODO: Write parser for res.text to scrape all listings.
+        list = soup.find_all('div', {'class': 'item-cnt clearfix gold'})
 
-        print(soup)
+        # Creating dataframe:
+        listings_data = pd.DataFrame(columns=['Address', 'Price', 'Beds',
+         'Baths', 'Size', 'Type'])
+
+        # Populating dataframe with elements found in list soup:
+        for element in list:
+            # Address: <div class = address-container>
+            Address = element.findAll('div', {'class': 'address-container'})[0].text
+
+            # Price: <div class = price>
+            Price = element.findAll('div', {'class': 'price'})[0].text
+
+            # Beds: <li data-label = Beds >
+            try:
+                Beds = element.findAll('li', {'data-label': 'Beds'})[0].text
+            except:
+                Beds = 'NaN'
+
+            # Baths: <li data-label = Baths>
+            try:
+                Baths = element.findAll('li', {'data-label': 'Baths'})[0].text
+            except:
+                Baths = 'NaN'
+
+            # Size: <li data-label = Sqft>
+            try:
+                Size = element.findAll('li', {'data-label': 'Sqft'})[0].text
+            except:
+                Size = 'NaN'
+
+            # Type: <li class = property-type ic-proptype>
+            try:
+                Type = element.findAll('li', {'class': 'property-type ic-proptype'})[0].text
+            except:
+                Type = 'NaN'
+
+
+            # Performing Raw data transformations and placing data into a pd.series:
+
+            # Address '     xxxx, xxxx, xxxx' -> xxxx, xxxx, xxxx:
+            Address = Address.replace('        ', '')
+            Address = Address.rstrip()
+
+            # Type '           xxx' -> xxxx:
+            Type = Type.replace(' ', '')
+            Type = Type.rstrip()
+
+            # Price '$xxx,xxx CAD' -> xxxxxx:
+            Price = Price.replace('$', '')
+            Price = Price.replace(',', '')
+            Price = Price.replace('CAD', '')
+            Price = Price.replace(' ', '')
+            Price = Price.rstrip()
+
+            # Beds 'x Beds' -> x:
+            Beds = Beds.replace('Beds', '')
+            Beds = Beds.replace(' ', '')
+            Beds = Beds.rstrip()
+
+            # Baths 'x Baths' -> x:
+            Baths = Baths.replace('Baths', '')
+            Baths = Baths.replace(' ', '')
+            Baths = Baths.rstrip()
+
+            # Size x,xxx Sqft -> xxxx:
+            Size = Size.replace(',', '')
+            Size = Size.replace('Sqft', '')
+            Size = Size.replace(' ', '')
+            Size = Size.rstrip()
+
+            # Creating Series:
+            print(Address, Beds, Baths, Size, Price, Type)
+
+
 
 
 class Toronto_raw_data(object):
