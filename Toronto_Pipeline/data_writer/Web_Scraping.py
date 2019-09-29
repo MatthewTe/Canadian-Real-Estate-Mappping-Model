6f2b06368cb8d13b5a55e3dc7e365dc0e0001c6a
MatthewTe <teelucksingh.matthew1@gmail.com>
2019-09-28 22:00:26 -0400

    Added TODO

--- Toronto_Pipeline/data_writer/Web_Scraping.py
+++ Toronto_Pipeline/data_writer/Web_Scraping.py
@@ -5,6 +5,41 @@ import bs4
 import pandas as pd
 
 
+class Point_2_Homes(object):
+    """
+    The Point_2_Homes object contains all the methods that scrape listings data
+    directly from 'https://www.point2homes.com/CA/Real-Estate-Listings/ON/Toronto.html'.
+
+    These methods will be called by the Toronto_raw_data() object to build the main
+    dataframe.
+    """
+
+    def get_listings(url):
+        '''Method generates a pandas dataframe containing all the real estate
+        listings data scraped from a given 'Point 2 Home' url using bs4 and
+        requests packages
+
+        Parameters
+        ----------
+        url : str
+            String representing the url of the 'Point 2 Homes' webpage
+
+        Returns
+        -------
+        listings_data : pandas dataframe
+            Dataframe containing all real estate listings data from the webpage
+        '''
+
+        # Connecting to webpage:
+        res = requests.get(url)
+        soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+        # Seaching for all instances of listings article {class : item-cnt clearfix gold}
+        # TODO: Write parser for res.text to scrape all listings.
+
+        print(soup)
+
+
 class Toronto_raw_data(object):
     """
     This object stores all of the methods and processes that scrapes the various
@@ -28,4 +63,17 @@ class Toronto_raw_data(object):
         self.Point_2_Homes = self.build_point_2_homes()
 
     def build_point_2_homes(self):
-        pass
+        '''This method calls on the point_2_homes object to build the pandas
+            dataframe containing all of the listings from 'Point 2 Homes.com'
+
+         Returns
+         --------
+         point_2_homes_data : pandas dataframe
+            Dataframe containing all the listings data scraped from 'Point 2 Homes
+            .com'
+
+        '''
+
+
+# Test Cases:
+Point_2_Homes.get_listings('https://www.point2homes.com/CA/Real-Estate-Listings/ON/Toronto.html')
